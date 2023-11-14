9ccb5cea25edd095f7a824e618a249cde8d4ba9b
mithrasridhar <mithugopal30@gmail.com>
2023-11-14 11:44:23 +0530

    final changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import {Button} from 'react-bootstrap'
-import Card from "react-bootstrap/Card";
+import { CreatePriceCard } from './CreatePriceCard';
 
 function App() {
 
@@ -127,64 +126,4 @@ function App() {
   );
 }
 
-function CreatePriceCard({plan,price,featureList}){
-console.log(featureList)
-return(
-  <div className ="cardcontainer">
-{/* <h1>
- {plan} {price} {featureList.map((feature) => <li>{feature.name}</li>)}
-</h1> */}
-<Card className="text-center">
-      <Card.Header>{plan}</Card.Header>
-      <Card.Body>
-        <Card.Title><b>{price}</b></Card.Title>
-        <hr></hr>
-        <Card.Text>
-
-        <ul className="fa-ul">
-             {
-    featureList.map((feature) =>{
-       return <li className={feature.isEnabled ? "" : "text-muted"}>               {/* this line is to disable the text (text-muted) */}
-    <span className="fa-li">
-        <i class={`fa ${feature.isEnabled ? 'fa-check' : 'fa-times'}`}></i>        {/* this line to font awesome icon ✔ and ✖ icon */}
-        </span>{feature.name}             {/* this line is to other content in card  */}
-    </li> 
-    })
-   }
- </ul>
-          {/* <ul className="fa-ul">
-            {
-                featureList.map((feature)=>{
-                //  console.log(feature.isEnabled )
-                  return <li className={ feature.isEnabled ? "": 'text-muted'}>
-                  <span>
-                    <i className={`fa ${feature.isEnabled ? 'fa-check':'fa-times'} `}></i>
-
-                  </span>{feature.name} 
-                  </li>
-                })
-            }
-</ul> */}
-        {/* {featureList.map((feature) => <ul>{feature.name} </ul>)} */}
-        </Card.Text>
-        <Button variant="primary">Button</Button>
-      </Card.Body>
-    </Card>
-
-{/* <div className="card" >
-  <div className="card-body">
-    <h5 className="card-title">{plan}</h5>
-    <p className="card-text">{price}.</p>
-  </div>
-  <ul className="list-group list-group-flush">
-  {featureList.map((feature) => <li>{feature.name}</li>)}
-  </ul>
-  <div className="card-body">
-   <button>Button</button>
-  </div>
-</div> */}
-
-</div>
-);
-}
 export default App;
